0c6ed6009752cfafc99b57d8cf5e295b17ec1a52
Yumi <andre.miethke74@gmail.com>
2026-08-14 14:06:27 +0200

    Attach video libraries to all learning fields

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,7 +3,8 @@ import { lf5 } from './lf5';
 import { lf6 } from './lf6';
 import { lf7 } from './lf7';
 import { lf8 } from './lf8';
+import { addVideoLibrary } from './videoExtras';
 
-export const modules = [lf4, lf5, lf6, lf7, lf8];
+export const modules = [lf4, lf5, lf6, lf7, lf8].map(addVideoLibrary);
 
 export { lf4, lf5, lf6, lf7, lf8 };
