1b6a9b3f121f46ce1f4d26102a810a39f730ca37
Yumi <andre.miethke74@gmail.com>
2026-08-14 17:03:52 +0200

    Apply deeper guided learning sections across LF1 to LF12

--- src/data/index.ts
+++ src/data/index.ts
@@ -12,6 +12,7 @@ import { lf11 } from './lf11';
 import { lf12 } from './lf12';
 import { enhanceLf4 } from './lf4Enhancements';
 import { enhanceLf5 } from './lf5Enhancements';
+import { enhanceDepth } from './depthEnhancements';
 import { addVideoLibrary } from './videoExtras';
 import type { LearningModule, TopicContent } from '@/types';
 
@@ -51,6 +52,7 @@ function withGitHubPagesAssets(module: LearningModule): LearningModule {
 export const modules = [lf1, lf2, lf3, lf4, lf5, lf6, lf7, lf8, lf9, lf10, lf11, lf12]
   .map(enhanceLf4)
   .map(enhanceLf5)
+  .map(enhanceDepth)
   .map(cleanModule)
   .map(withGitHubPagesAssets)
   .map(addVideoLibrary);
