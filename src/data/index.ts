f4f4e4f9a4c72412a393f87c79f5f8018fd1470c
Yumi <andre.miethke74@gmail.com>
2026-08-14 15:22:54 +0200

    Complete MFA curriculum with learning fields 1 to 12

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,11 +1,40 @@
+import { lf1 } from './lf1';
+import { lf2 } from './lf2';
+import { lf3 } from './lf3';
 import { lf4 } from './lf4';
 import { lf5 } from './lf5';
 import { lf6 } from './lf6';
 import { lf7 } from './lf7';
 import { lf8 } from './lf8';
 import { lf9 } from './lf9';
+import { lf10 } from './lf10';
+import { lf11 } from './lf11';
+import { lf12 } from './lf12';
+import { enhanceLf4 } from './lf4Enhancements';
 import { addVideoLibrary } from './videoExtras';
-import type { LearningModule } from '@/types';
+import type { LearningModule, TopicContent } from '@/types';
+
+const supportedContentTypes = new Set<TopicContent['type']>([
+  'text',
+  'info',
+  'warning',
+  'table',
+  'list',
+  'definition',
+  'heading',
+  'image',
+  'video',
+]);
+
+function cleanModule(module: LearningModule): LearningModule {
+  return {
+    ...module,
+    topics: module.topics.map(topic => ({
+      ...topic,
+      content: topic.content.filter(block => supportedContentTypes.has(block.type)),
+    })),
+  };
+}
 
 function withGitHubPagesAssets(module: LearningModule): LearningModule {
   const heroImage = module.heroImage.startsWith('/') && !module.heroImage.startsWith('//')
@@ -18,8 +47,10 @@ function withGitHubPagesAssets(module: LearningModule): LearningModule {
   };
 }
 
-export const modules = [lf4, lf5, lf6, lf7, lf8, lf9]
+export const modules = [lf1, lf2, lf3, lf4, lf5, lf6, lf7, lf8, lf9, lf10, lf11, lf12]
+  .map(enhanceLf4)
+  .map(cleanModule)
   .map(withGitHubPagesAssets)
   .map(addVideoLibrary);
 
-export { lf4, lf5, lf6, lf7, lf8, lf9 };
+export { lf1, lf2, lf3, lf4, lf5, lf6, lf7, lf8, lf9, lf10, lf11, lf12 };
